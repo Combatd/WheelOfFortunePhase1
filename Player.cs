cf71fd61c4c0e4236fba1d2db72cc8f908e08a3a
Mark Calvelo <combatdstar@gmail.com>
2021-03-11 11:42:44 -0800

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Player
+/// 
+/// This is the Player class.
+/// It holds the Player's name.
+/// 
+/// name: String
+/// guessPhrase(): String
+/// guessChar(): Char
+/// 
+/// #guessPhrase:
+/// Allows the player to guess a phrase. Returns a string.
+/// 
+/// #guessChar:
+/// Allows the player to guess a one letter char in a phrase. Returns a char.
+/// </summary>
+public class Player
+{
+	// instance variables
+	// public string memberName; // name of the player
+
+	public Player()
+	{
+		// this.name = name;
+	}
+
+	public string guessPhrase()
+    {
+		/*
+		 * #guessPhrase() allows the Player to guess the phrase to win the round.
+		 * it should grab a string that is read from the Console.ReadLine().
+		 * This may be invoked in TurnManager#promptGuess.
+		 * It returns a string from the user input.
+		 */
+    }
+
+	public char guessChar()
+    {
+		/*
+		 * #guessChar allows the Player to guess a character in a phrase.
+		 * It should grab a char that is read from the Console.ReadLine().
+		 * This may be invoked in TurnManager#promptGuess.
+		 * It returns a char from the user input.
+		 */
+    }
+}
